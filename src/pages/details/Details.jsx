7614911a367ba31f97d17f724fcc578d1fbe0a72
asefibrahim <strongman01666@gmail.com>
2023-08-10 19:58:25 +0600

    set up details page

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import './styles.scss'
+import DetailsBanner from './detailsBanner/DetailsBanner';
 const Details = () => {
     return (
         <div>
-
+            <DetailsBanner />
         </div>
     );
 };
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,7 @@ import './styles.scss'
 import HeroBanner from './heroBanner/HeroBanner';
 import Trending from './trending/Trending';
 import Popular from './popular/Popular';
+import TopRated from './topRated/TopRated';
 const Home = () => {
     return (
         <div className='homePage'>
@@ -10,6 +11,7 @@ const Home = () => {
             ></HeroBanner>
             <Trending></Trending>
             <Popular />
+            <TopRated />
         </div>
     );
 };
