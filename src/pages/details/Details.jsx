ef23a00ec53cb80db55b695853d9e369ce5c55d2
asefibrahim <strongman01666@gmail.com>
2023-08-10 20:33:06 +0600

    added official vedio section as well as cast details

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -3,6 +3,8 @@ import './styles.scss'
 import DetailsBanner from './detailsBanner/DetailsBanner';
 import { useParams } from 'react-router-dom';
 import useFetch from '../../hooks/useFetch';
+import Cast from './cast/Cast';
+import VideosSection from './vediosSection/VediosSection';
 const Details = () => {
     const { mediaType, id } = useParams();
     const { data, loading } = useFetch(`/${mediaType}/${id}/videos`);
@@ -14,6 +16,8 @@ const Details = () => {
     return (
         <div>
             <DetailsBanner video={data?.results?.[0]} crew={credits?.crew} />
+            <Cast data={credits?.cast} loading={creditsLoading} />
+            <VideosSection data={data} loading={loading} />
         </div>
     );
 };
