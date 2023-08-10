00d40d376d86e101019f58046e70b6026a37a2f8
asefibrahim <strongman01666@gmail.com>
2023-08-10 17:44:01 +0600

    added popular section of movies and tv shows

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './styles.scss'
 import HeroBanner from './heroBanner/HeroBanner';
 import Trending from './trending/Trending';
+import Popular from './popular/Popular';
 const Home = () => {
     return (
         <div className='homePage'>
             <HeroBanner
             ></HeroBanner>
             <Trending></Trending>
+            <Popular />
         </div>
     );
 };
