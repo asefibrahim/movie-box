a9a18f3d135ac8d4c12e1dbb696428286c3ae20e
asefibrahim <strongman01666@gmail.com>
2023-08-10 14:29:39 +0600

    Setting dynamic Bg image and complete heroSection

--- src/App.jsx
+++ src/App.jsx
@@ -20,11 +20,23 @@ function App() {
   })
   console.log(url);
   useEffect(() => {
-    apiTesting()
-  }, [])
+    fetchApiConfig();
 
-  const apiTesting = () => {
-    fetchDataFromApi('/movie/popular').then(res => dispatch(getApiConfiguration(res)))
+  }, []);
+
+  const fetchApiConfig = () => {
+    fetchDataFromApi("/configuration").then((res) => {
+      console.log(res);
+
+      const url = {
+        backdrop: res.images.secure_base_url + "original",
+        poster: res.images.secure_base_url + "original",
+        profile: res.images.secure_base_url + "original",
+      };
+
+      dispatch(getApiConfiguration(url));
+    });
+
 
 
   }
